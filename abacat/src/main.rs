f29fe43aef73e852ec422d9e26627dee463696d0
mini-ninja-64 <mini-ninja-64@users.noreply.github.com>
2026-04-06 17:33:15 +0100

    Switch to git version of cxx-qt to fix linux build

--- abacat/src/main.rs
+++ abacat/src/main.rs
@@ -5,6 +5,7 @@ use cxx_qt_lib::{QGuiApplication, QQmlApplicationEngine, QUrl};
 
 // TODO: BUGS
 // - empty lines are thinner than lines with content by like 1 or 2 pixels :sob:
+// - Linux freezes when going from 10000 lines of error to 10000 lines having a value, but updating a value seems ok?
 
 fn main() {
     let mut app = QGuiApplication::new();
